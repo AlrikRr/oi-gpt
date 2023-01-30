a7c9cfbc388acffc044aeff829eb8b957a33dba8
Adrien Lasalle <adrien@Adriens-MacBook-Pro.local>
2023-01-30 18:39:17 -0500

    First Init - Oi GPT

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# oi-gpt
-Oi, GPT ! 
+# Oi, GPT ! 
+
+This Python script uses the OpenAI API to generate responses to questions using CHATGPT. It supports three different modes of operation, specified by a single command-line argument:
+
+- `-h`: Display a help menu that explains how to use the script.
+- `-c`: Start an infinite loop to ask questions and receive answers. The loop continues until the user enters `exit`.
+- `phrase`: A string that represents the question to ask CHATGPT.
+
+## Requirements
+
+- Python 3.x
+- OpenAI API key
+- The `openai` library: `pip install openai`
+- Clipboard library for your operating system: `win32clipboard` for Windows, `subprocess` for macOS and Linux
+
+## Usage
+
+```bash
+python script.py [-c | phrase]
+```
+- `-c`: Start an infinite loop to ask questions and receive answers.
+- `phrase`: The phrase to ask CHATGPT. Enclose the phrase in quotes if it contains spaces.
+
+## Output
+
+The script outputs the response from CHATGPT to the console and saves it to the system clipboard.
+
+## Note
+
+This script was generated using OpenAI's CHATGPT language model.
---
+++ oi-gpt.py
@@ -0,0 +1,56 @@
+import openai
+import sys
+
+if sys.platform == "win32":
+    import win32clipboard
+    def to_clipboard(text):
+        win32clipboard.OpenClipboard()
+        win32clipboard.EmptyClipboard()
+        win32clipboard.SetClipboardText(text)
+        win32clipboard.CloseClipboard()
+elif sys.platform == "darwin":
+    import subprocess
+    def to_clipboard(text):
+        process = subprocess.Popen(
+            "pbcopy", env={"LANG": "en_US.UTF-8"}, stdin=subprocess.PIPE
+        )
+        process.communicate(text.encode("utf-8"))
+else:
+    raise Exception("Unsupported platform")
+
+openai.api_key = "YOUR-API-KEY"
+
+def generate_response(prompt):
+    completions = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    message = completions.choices[0].text
+    return message
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        raise Exception("Invalid number of arguments")
+
+    if sys.argv[1] == "-h":
+        print("Usage: python script.py [-c | phrase]")
+        print("-c: start an infinite loop to ask questions and receive answers")
+        print("phrase: the phrase to ask CHATGPT (enclosed in quotes if it contains spaces)")
+    elif sys.argv[1] != "-c":
+        prompt = sys.argv[1]
+        response = generate_response(prompt)
+        print(response)
+        to_clipboard(response)
+    else:
+        while True:
+            prompt = input("Enter a question (or 'exit' to exit): ")
+            if prompt.strip().lower() == "exit":
+                break
+            response = generate_response(prompt)
+            print(response)
+            to_clipboard(response)
+
